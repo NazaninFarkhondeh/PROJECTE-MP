61dfcc223d54464aaeef16c18290ba8689faf843
1714503 <148911376+1714503@users.noreply.github.com>
2024-04-18 23:32:30 +0200

    Update Tauler.cpp

--- Tauler.cpp
+++ Tauler.cpp
@@ -1,1 +1,23 @@
 #include "Tauler.h"
+#include <iostream>
+
+Tauler::Tauler() {
+    caselles.resize(MAX_FILA, std::vector<char>(MAX_COL, ' '));
+}
+
+void Tauler::mostrar() {
+    for (int i = 0; i < MAX_FILA; ++i) {
+        for (int j = 0; j < MAX_COL; ++j) {
+            std::cout << caselles[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Tauler::setCasella(int fila, int columna, char valor) {
+    caselles[fila][columna] = valor;
+}
+
+bool Tauler::casellaOcupada(int fila, int columna) const {
+    return caselles[fila][columna] != ' ';
+}
