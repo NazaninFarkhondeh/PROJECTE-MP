af23627b94e000e1235bb080b35f39252c1d287c
1714503 <148911376+1714503@users.noreply.github.com>
2024-04-18 23:58:19 +0200

    afegit escriuTauler Tauler.cpp

--- Tauler.cpp
+++ Tauler.cpp
@@ -21,3 +21,18 @@ void Tauler::setCasella(int fila, int columna, char valor) {
 bool Tauler::casellaOcupada(int fila, int columna) const {
     return caselles[fila][columna] != ' ';
 }
+
+void Tauler::escriuTauler(const std::string& nomFitxer) {
+    std::ofstream fitxer(nomFitxer);
+
+    if (fitxer.is_open()) {
+        for (int i = 0; i < MAX_FILA; ++i) {
+            for (int j = 0; j < MAX_COL; ++j) {
+                fitxer << caselles[i][j] << " ";
+            }
+            fitxer << std::endl;
+        }
+        fitxer.close();
+    } else {
+        std::cout << "No s'ha pogut obrir el fitxer per escriure." << std::endl;
+    }
