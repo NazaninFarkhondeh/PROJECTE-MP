ae4030c458249aaec41103acfa706e13151f5ff1
1714503 <148911376+1714503@users.noreply.github.com>
2024-04-18 23:33:20 +0200

    Update Tauler.h

--- Tauler.h
+++ Tauler.h
@@ -1,11 +1,21 @@
 #ifndef TAULER_H
 #define TAULER_H
 #include "Figura.h"
+#include <vector>
 
 const int MAX_FILA = 8;
 const int MAX_COL = 8;
 
 class Tauler
 {
+    
+    private:
+    std::vector<std::vector<char>> caselles; // Matriz para representar el tauler
+
+public:
+    Tauler(); // Constructor inicializa el tauler con casillas vacías
+    void mostrar(); // Método para mostrar el tauler en la consola
+    void setCasella(int fila, int columna, char valor); // Método para establecer el valor de una casilla
+    bool casellaOcupada(int fila, int columna) const; // Verificar si una casilla está ocupada
 };
 #endif
