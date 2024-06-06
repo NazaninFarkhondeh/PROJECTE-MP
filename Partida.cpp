15e6d2c7f1ba986b26f32195bb91e06d15282ecb
1714503 <148911376+1714503@users.noreply.github.com>
2024-06-06 09:13:27 +0200

    Update Partida.cpp

--- Partida.cpp
+++ Partida.cpp
@@ -1,10 +1,12 @@
 #include "Partida.h"
 #include "InfoJoc.h"
 #include "GraphicManager.h"
+#define MIDA
 
 Partida::Partida()
 {
     m_temps = 0;
+    int m_forma[100][100];
     // Inicializar el atributo m_forma
     for (int i = 0; i < MIDA; i++)
         for (int j = 0; j < MIDA; j++)
@@ -14,26 +16,29 @@ Partida::Partida()
     m_forma[1][0] = COLOR_BLAUFOSC;
     m_forma[1][1] = COLOR_BLAUFOSC;
     m_forma[1][2] = COLOR_BLAUFOSC;
-    m_fila = 1;
-    m_columna = 5;
+    int m_fila = 1;
+    int m_columna = 5;
 }
 
 void Partida::actualitza(double deltaTime)
 {
+    int m_fila;
+    int m_columna;
+    int m_forma[100][100];
     // Incrementar el tiempo acumulado
     m_temps += deltaTime;
-    
+
     // Actualizar la posición de la figura cada 0.5 segundos
     if (m_temps > 0.5) {
         if (m_fila < N_FILES_TAULER) // Verificar si no hemos llegado al fondo del tablero
             m_fila++; // Mover la figura hacia abajo
         m_temps = 0.0; // Reiniciar el tiempo
     }
-    
+
     // Dibujar el fondo y el tablero
     GraphicManager::getInstance()->drawSprite(GRAFIC_FONS, 0, 0, false);
     GraphicManager::getInstance()->drawSprite(GRAFIC_TAULER, POS_X_TAULER, POS_Y_TAULER, false);
-    
+
     // Dibujar la figura en su posición actual
     for (int i = 0; i < MIDA; i++) {
         for (int j = 0; j < MIDA; j++) {
@@ -43,12 +48,12 @@ void Partida::actualitza(double deltaTime)
                 float posY = POS_Y_TAULER + ((m_fila - 1 + i) * MIDA_QUADRAT);
                 // Dibujar el cuadrado correspondiente al color de la figura
                 switch (m_forma[i][j]) {
-                    case COLOR_BLAUFOSC:
-                        GraphicManager::getInstance()->drawSprite(GRAFIC_QUADRAT_BLAUFOSC, posX, posY, false);
-                        break;
+                case COLOR_BLAUFOSC:
+                    GraphicManager::getInstance()->drawSprite(GRAFIC_QUADRAT_BLAUFOSC, posX, posY, false);
+                    break;
                     // Agregar casos para otros colores si es necesario
-                    default:
-                        break;
+                default:
+                    break;
                 }
             }
         }
